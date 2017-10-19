0de5d4303812e86aa14bf7f33d64208cca28eec3
CHyde <blimple10@gmail.com>
2017-10-19 10:18:48 -0700

    Programming Exercise Question 2
    Prints information about myself to console.

---
+++ AboutMe.java
@@ -0,0 +1,14 @@
+/** Program: Hello World Program
+* File: AboutMe.java
+* Summary: Prints information about myself to the console.
+* Author: Chris Hyde
+* Date: October 19, 2017 
+**/
+
+public class AboutMe{
+	public static void main(String[] args){
+		System.out.println("My name is Chris Hyde.");
+		System.out.println("My favorit color is Forest Green.");
+		System.out.println("If I won the lottery, I would open my own game development studio.");
+	}
+}
